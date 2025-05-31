f87c9eae41cbf8cfd363888da699d486c2bad023
jigneshsurat009 <jigneshsurat009@gmail.com>
2025-05-31 16:52:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "15964777"))
 API_HASH = os.environ.get("API_HASH", "ef448f85b780cdf26f8ffe390a5d8262")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7478579847:AAHplgR271OZwVOCsRqra3GkJ0Rv5ansZvE")
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://premiumcourseonline:Jl7RCixxaTK0E1ER@cluster0.yavtoms.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.environ.get("OWNER_ID", "944358553"))
